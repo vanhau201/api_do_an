e479a82ad0d7dc15ae29d3d2540734540066c029
vanhau201 <conghau12a5@gmail.com>
2022-01-28 17:40:00 +0700

    update

--- api/views.py
+++ api/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import numpy as np
-import urllib
 import cv2
 import torch
 # Create your views here.
